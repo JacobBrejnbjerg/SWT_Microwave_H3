d587c6571bed2c26fe94bfb086f2b5762f12127b
Jacob Mørk Brejnbjerg <au661789@uni.au.dk>
2022-04-17 19:21:40 +0200

    Added unit tests to verify that the new MaxPower parameter can be set and it also affects the behaviour of the PowerTube

--- Microwave.Test.Unit/PowerTubeTest.cs
+++ Microwave.Test.Unit/PowerTubeTest.cs
@@ -61,5 +61,45 @@ namespace Microwave.Test.Unit
             uut.TurnOn(50);
             Assert.Throws<System.ApplicationException>(() => uut.TurnOn(60));
         }
+
+        /////////// New unit tests ///////////
+        // Makes sure that the maxpower can be changed, and the outside limits of maxpower also changes
+        [TestCase(500, -1)]
+        [TestCase(500, 0)]
+        [TestCase(500, 501)]
+        [TestCase(500, 2000)]
+        [TestCase(1000, -1)]
+        [TestCase(1000, 0)]
+        [TestCase(1000, 1001)]
+        [TestCase(1000, 1002)]
+        public void TurnOn_OutsideMaxPower_ExceptionThrown(int maxPower, int turnOnPower)
+        {
+            uut = new PowerTube(output, maxPower);
+
+            Assert.Throws<System.ArgumentOutOfRangeException>(() => uut.TurnOn(turnOnPower));
+        }
+
+        // Makes sure that the maxpower can be changed, and the within limits also changes
+        [TestCase(500, 1)]
+        [TestCase(500, 2)]
+        [TestCase(500, 499)]
+        [TestCase(500, 500)]
+        [TestCase(1000, 1)]
+        [TestCase(1000, 2)]
+        [TestCase(1000, 999)]
+        [TestCase(1000, 1000)]
+        public void TurnOn_WithinMaxPower_NoExceptionThrown(int maxPower, int turnOnPower)
+        {
+            uut = new PowerTube(output, maxPower);
+
+            Assert.DoesNotThrow(() => uut.TurnOn(turnOnPower));
+        }
+
+        [Test]
+        public void Initialization_MaxPower_IsDefault700W()
+        {
+            uut = new PowerTube(output);
+            Assert.That(uut.MaxPower == 700);
+        }
     }
 }
