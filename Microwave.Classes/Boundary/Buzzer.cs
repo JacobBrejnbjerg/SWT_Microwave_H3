9a025ed8fe607df6b684f9d0a57fe1e55e598791
AlExIsNoTaVaIlAbL <98851343+AlExIsNoTaVaIlAbL@users.noreply.github.com>
2022-04-26 16:33:15 +0200

    Added feature buzzer

---
+++ Microwave.Classes/Boundary/Buzzer.cs
@@ -0,0 +1,22 @@
+﻿using Microwave.Classes.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microwave.Classes.Boundary
+{
+    public class Buzzer : IBuzzer
+    {
+        private IOutput myOutput;
+
+        public Buzzer(IOutput output)
+        {
+            myOutput = output;
+        }
+
+        public void BurstBuzz()
+        {
+            myOutput.OutputLine("Buzz Buzz Buzz");
+        }
+    }
+}
